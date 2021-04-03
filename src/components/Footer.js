6a5204d1743d0b87238e8c921e48fc81f6fab262
Yilmer Avila <yilmer0001@hotmail.com>
2021-04-03 15:41:09 -0500

    Footer creation

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="main-footer">
+            <div className="float-right d-none d-sm-block">
+                <b>Version</b> 3.1.0
+            </div>
+            <strong>Copyright &copy; 2021 <a href="#">Repaso henry</a>.</strong> All rights reserved.
+        </footer>
+    )
+}
+
+export default Footer
