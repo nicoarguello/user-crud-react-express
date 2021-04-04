4138e2756d140b00a111f61a44986011ac97eed1
Xaldersoul <benjamin.g.guerra@hotmail.com>
2021-04-04 17:22:58 -0300

    create user

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,16 +1,19 @@
 import Title from '../components/MainContent/Title'
 import Content from '../components/MainContent/Content'
+import CreateUser from "../components/MainContent/CreateUser"
+import { Route } from "react-router-dom"
 
 const MainContainer = (props) => {
 
-    return(
+    return (
         <section className="content">
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-12">
                         <div className="card">
-                           <Title />
-                           <Content />
+                            <Title />
+                            <Content />
+                            <Route path="/createUser" component={CreateUser} />
                         </div>
                     </div>
                 </div>
